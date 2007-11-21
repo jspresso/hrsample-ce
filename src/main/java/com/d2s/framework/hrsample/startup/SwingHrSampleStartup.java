4a3e82887ac6cb09b9f2d99a0fd763a585138f0f
Vincent Vandenschrick <vvandens@jspresso.org>
2007-11-21 18:16:56 +0000

    HR sample startup

---
+++ src/main/java/com/d2s/framework/hrsample/startup/SwingHrSampleStartup.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2005 Design2see. All rights reserved.
+ */
+package com.d2s.framework.hrsample.startup;
+
+import com.d2s.framework.application.frontend.startup.swing.SwingStartup;
+
+/**
+ * Swing HR sample startup class.
+ * <p>
+ * Copyright 2005 Design2See. All rights reserved.
+ * <p>
+ * 
+ * @version $LastChangedRevision$
+ * @author Vincent Vandenschrick
+ */
+public class SwingHrSampleStartup extends SwingStartup {
+
+  /**
+   * Returns the "sample-swing-context" value.
+   * <p>
+   * {@inheritDoc}
+   */
+  @Override
+  protected String getApplicationContextKey() {
+    return "hrsample-swing-context";
+  }
+}
