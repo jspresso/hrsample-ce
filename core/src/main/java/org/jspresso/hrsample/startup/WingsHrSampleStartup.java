cc2e4488cf6fb2ef8735839678ac930bf39164bb
Vincent Vandenschrick <vvandens@jspresso.org>
2008-04-24 16:14:02 +0000

    Completed Webapp structure.

---
+++ core/src/main/java/org/jspresso/hrsample/startup/WingsHrSampleStartup.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2005-2008 Vincent Vandenschrick. All rights reserved.
+ */
+package org.jspresso.hrsample.startup;
+
+import org.jspresso.framework.application.startup.wings.WingsStartup;
+
+/**
+ * Wings HR sample startup class.
+ * <p>
+ * Copyright (c) 2005-2008 Vincent Vandenschrick. All rights reserved.
+ * <p>
+ * 
+ * @version $LastChangedRevision$
+ * @author Vincent Vandenschrick
+ */
+public class WingsHrSampleStartup extends WingsStartup {
+
+  /**
+   * Returns the "hrsample-wings-context" value.
+   * <p>
+   * {@inheritDoc}
+   */
+  @Override
+  protected String getApplicationContextKey() {
+    return "hrsample-wings-context";
+  }
+}
---
+++ webapp/src/main/java/org/jspresso/hrsample/webapp/TestDataContextListener.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2005-2008 Vincent Vandenschrick. All rights reserved.
+ */
+package org.jspresso.hrsample.webapp;
+
+import org.jspresso.framework.application.startup.development.AbstractTestDataContextListener;
+import org.jspresso.hrsample.startup.development.HrTestDataPersister;
+import org.springframework.beans.factory.BeanFactory;
+
+/**
+ * A simple listener to hook in webapp startup and persist sample data.
+ * <p>
+ * Copyright 2005-2008 BlueVox. All rights reserved.
+ * <p>
+ * 
+ * @version $LastChangedRevision$
+ * @author Vincent Vandenschrick
+ */
+public class TestDataContextListener extends AbstractTestDataContextListener {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void persistTestData(BeanFactory beanFactory) {
+    new HrTestDataPersister(beanFactory).persistTestData();
+  }
+
+}
