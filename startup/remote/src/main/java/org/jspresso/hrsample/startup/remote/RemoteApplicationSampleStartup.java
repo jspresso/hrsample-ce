5f6467915945999983f021ea9759317827599082
Vincent Vandenschrick <vvandens@jspresso.org>
2009-02-06 17:40:18 +0000

    Remote application startup

---
+++ startup/remote/src/main/java/org/jspresso/hrsample/startup/remote/RemoteApplicationSampleStartup.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2005-2008 Vincent Vandenschrick. All rights reserved.
+ */
+package org.jspresso.hrsample.startup.remote;
+
+import org.jspresso.framework.application.startup.remote.RemoteStartup;
+
+/**
+ * Remote HR sample startup class.
+ * <p>
+ * Copyright (c) 2005-2008 Vincent Vandenschrick. All rights reserved.
+ * <p>
+ * 
+ * @version $LastChangedRevision$
+ * @author Vincent Vandenschrick
+ */
+public class RemoteApplicationSampleStartup extends RemoteStartup {
+
+  /**
+   * Returns the "hrsample-remote-context" value.
+   * <p>
+   * {@inheritDoc}
+   */
+  @Override
+  protected String getApplicationContextKey() {
+    return "hrsample-remote-context";
+  }
+}
