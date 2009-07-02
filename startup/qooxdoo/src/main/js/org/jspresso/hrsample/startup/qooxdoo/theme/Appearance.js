b730a97d272eb823bf3f73c6a122056053149d78
Vincent Vandenschrick <vvandens@jspresso.org>
2009-07-02 18:27:52 +0000

    Qooxdoo frontend for HR sample 1st commit

---
+++ startup/qooxdoo/src/main/js/org/jspresso/hrsample/startup/qooxdoo/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("org.jspresso.hrsample.startup.qooxdoo.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ startup/qooxdoo/src/main/js/org/jspresso/hrsample/startup/qooxdoo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("org.jspresso.hrsample.startup.qooxdoo.theme.Theme",
+{
+  meta :
+  {
+    color : org.jspresso.hrsample.startup.qooxdoo.theme.Color,
+    decoration : org.jspresso.hrsample.startup.qooxdoo.theme.Decoration,
+    font : org.jspresso.hrsample.startup.qooxdoo.theme.Font,
+    icon : org.jspresso.hrsample.startup.qooxdoo.theme.Tango,
+    appearance : org.jspresso.hrsample.startup.qooxdoo.theme.Appearance
+  }
+});
