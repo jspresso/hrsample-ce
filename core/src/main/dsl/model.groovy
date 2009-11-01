9a29dc68aa1c9386527711ad0fee042724055249
Vincent Vandenschrick <vvandens@jspresso.org>
2009-11-01 17:28:39 +0000

    sjs update

--- core/src/main/dsl/model.groovy
+++ core/src/main/dsl/model.groovy
@@ -6,42 +6,32 @@ domainBuilder.Project('hrsample') {
   namespace('org.jspresso.hrsample') {
     Domain {
       Interface ('Nameable') {
-        properties {
-          string_64 'name',mandatory:true
-        }
+        string_64 'name',mandatory:true
       }
       
       Interface('Traceable',
-        interceptors:'TraceableLifecycleInterceptor',
-        icon:'traceable-48x48.png',
-        uncloned:['createTimestamp', 'lastUpdateTimestamp']) {
-          properties {
-            date_time 'createTimestamp', readOnly:true
-            date_time 'lastUpdateTimestamp', readOnly:true
-          }
-        }
+          interceptors:'TraceableLifecycleInterceptor',
+          icon:'traceable-48x48.png',
+          uncloned:['createTimestamp', 'lastUpdateTimestamp']) {
+        date_time 'createTimestamp', readOnly:true
+        date_time 'lastUpdateTimestamp', readOnly:true
+      }
       
       Entity('City',
           extend:'Nameable',
           icon:'city-48x48.png') {
-        properties {
-          string_10 'zip'
-        }
+        string_10 'zip'
       }
       
       Component('ContactInfo') {
-        properties {
-          string_256 'address'
-          reference  'city', ref:'City'
-          string_32  'phone'
-          string_128 'email', regex:"[\\w\\-\\.]*@[\\w\\-\\.]*", regexSample:'contact@acme.com'
-        }
+        string_256 'address'
+        reference  'city', ref:'City'
+        string_32  'phone'
+        string_128 'email', regex:"[\\w\\-\\.]*@[\\w\\-\\.]*", regexSample:'contact@acme.com'
       }
       
       Entity('Event',extend:'Traceable'){
-        properties {
-          text 'text', maxLength:2048 , id:'Event-text'
-        }
+        text 'text', maxLength:2048 , id:'Event-text'
       }
       
       Entity ('Employee',
@@ -53,36 +43,32 @@ domainBuilder.Project('hrsample') {
           uncloned:['managedOu','ssn'],
           ordering:['name':'ASCENDING'],
           rendered:['name','firstName','gender','birthDate','age','ssn','salary','contact','married','preferredColor','photo','company','createTimestamp','lastUpdateTimestamp']) {
-        properties{
-          string_32 'firstName', mandatory:true, processors:'FirstNameProcessor'
-          string_10 'ssn', regex:"[\\d]{10}", regexSample:'0123456789', unicityScope:'empSsn'
-          date 'birthDate', processors:'BirthDateProcessor'
-          date 'hireDate'
-          enumeration 'gender', enumName:'GENDER', maxLength:1, mandatory:true, valuesAndIcons:[
-              'M':'male-48x48.png',
-              'F':'female-48x48.png']
-          reference 'contact', ref:'ContactInfo', id:'contact'
-          color 'preferredColor'
-          bool 'married'
-          decimal 'salary', minValue:0, usingBigDecimal:true
-          list 'events', composition:true, ref:'Event'
-          set 'teams', ref:'Team'
-          reference 'company', ref:'Company', mandatory:true, reverse:'Company-employees'
-          reference 'managedOu', ref:'OrganizationalUnit', reverse:'OrganizationalUnit-manager'
-          integer 'age', minValue:0, maxValue:150, readOnly:true, useExtension:true
-          binary 'photo', maxLength:1048576, id:'Employee-photo', fileFilter:['images':['.jpg','.bmp']]
-        }
+        string_32 'firstName', mandatory:true, processors:'FirstNameProcessor'
+        string_10 'ssn', regex:"[\\d]{10}", regexSample:'0123456789', unicityScope:'empSsn'
+        date 'birthDate', processors:'BirthDateProcessor'
+        date 'hireDate'
+        enumeration 'gender', enumName:'GENDER', maxLength:1, mandatory:true, valuesAndIcons:[
+            'M':'male-48x48.png',
+            'F':'female-48x48.png']
+        reference 'contact', ref:'ContactInfo', id:'contact'
+        color 'preferredColor'
+        bool 'married'
+        decimal 'salary', minValue:0, usingBigDecimal:true
+        list 'events', composition:true, ref:'Event'
+        set 'teams', ref:'Team'
+        reference 'company', ref:'Company', mandatory:true, reverse:'Company-employees'
+        reference 'managedOu', ref:'OrganizationalUnit', reverse:'OrganizationalUnit-manager'
+        integer 'age', minValue:0, maxValue:150, readOnly:true, useExtension:true
+        binary 'photo', maxLength:1048576, id:'Employee-photo', fileFilter:['images':['.jpg','.bmp']]
       }
       
       Entity('Company',
           extend:['Nameable', 'Traceable'],
           icon:'company-48x48.png',
           rendered:['name','contact','createTimestamp','lastUpdateTimestamp']) {
-        properties{
-          refId 'contact', id:'contact'
-          set 'departments', composition:true, ref:'Department'
-          set 'employees', composition:true, ref:'Employee'
-        }
+        refId 'contact', id:'contact'
+        set 'departments', composition:true, ref:'Department'
+        set 'employees', composition:true, ref:'Employee'
       }
       
       Entity('OrganizationalUnit',
@@ -90,34 +76,28 @@ domainBuilder.Project('hrsample') {
           purelyAbstract:true,
           processor :'OrganizationalUnitPropertyProcessors',
           extension :'OrganizationalUnitExtension') {
-          properties {
-            string_6 'ouId', regex:"[A-Z]{2}-[\\d]{3}", regexSample:'AB-123', mandatory:true
-            refId 'contact', id:'contact'
-            reference 'manager', id:'OrganizationalUnit-manager', ref:'Employee', mandatory:true, processors:['ManagerProcessor'], initializationMapping:['company':'company']
-            reference 'company', ref:'Company', useExtension:true
-          }
-        }
+        string_6 'ouId', regex:"[A-Z]{2}-[\\d]{3}", regexSample:'AB-123', mandatory:true
+        refId 'contact', id:'contact'
+        reference 'manager', id:'OrganizationalUnit-manager', ref:'Employee', mandatory:true, processors:['ManagerProcessor'], initializationMapping:['company':'company']
+        reference 'company', ref:'Company', useExtension:true
+      }
       
       Entity('Department',
           extend:'OrganizationalUnit',
           extension :'DepartmentExtension',
           icon:'department-48x48.png',
           rendered:['ouId','name','manager','contact','createTimestamp','lastUpdateTimestamp']) {
-        properties {
-          reference 'company', ref:'Company', reverse:'Company-departments', mandatory:true
-          set 'teams', composition:true, ref:'Team'
-          set 'employees', ref:'Employee', useExtension:true
-        }
+        reference 'company', ref:'Company', reverse:'Company-departments', mandatory:true
+        set 'teams', composition:true, ref:'Team'
+        set 'employees', ref:'Employee', useExtension:true
       }
       
       Entity('Team',
           extend:['OrganizationalUnit'],
           icon:'team-48x48.png',
           rendered:['ouId','name','manager','contact']){
-        properties {
-          reference 'department', ref:'Department', mandatory:true, reverse:'Department-teams'
-          set 'teamMembers', ref:'Employee', reverse:'Employee-teams'
-        }
+        reference 'department', ref:'Department', mandatory:true, reverse:'Department-teams'
+        set 'teamMembers', ref:'Employee', reverse:'Employee-teams'
       }
     }
   }
