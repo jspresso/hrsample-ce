8e8308209ae04d692ebfd8207c07f78b7cb49d9f
Vincent Vandenschrick <vvandens@jspresso.org>
2010-06-01 09:59:17 +0000

    LOV view single selection enforced.

--- core/src/main/dsl/view.groovy
+++ core/src/main/dsl/view.groovy
@@ -2,7 +2,7 @@ template 'form',
   parent:'decoratedView',
   labelsPosition:'ABOVE',
   columnCount:2
-  
+
 template 'table',
   parent:'decoratedView'
 
@@ -20,15 +20,15 @@ form 'Company.pane',
 treeNode 'Department-teams.treeNode',
   rendered:'ouId',
   actionMap:'masterDetail'
-  
+
 treeNode 'Department-employees.treeNode',
   rendered:'name',
   actionMap:'masterDetail'
-      
+
 treeNode 'Company-employees.treeNode',
   rendered:'name',
   actionMap:'masterDetail'
-            
+
 treeNode 'Company-departments.treeNode',
   rendered:'ouId',
   actionMap:'masterDetail'
@@ -36,12 +36,10 @@ treeNode 'Company-departments.treeNode',
 tree('Company.tree',
   rendered:'name',
   icon:'structure-48x48.png') {
-  subTree('Company-employees.treeNode')
-  subTree('Company-departments.treeNode') {
-    subTree('Department-teams.treeNode')
-    //subTree('Department-employees.treeNode')
+    subTree('Company-employees.treeNode')
+    subTree('Company-departments.treeNode') { subTree('Department-teams.treeNode') //subTree('Department-employees.treeNode')
+    }
   }
-}
 
 tabs 'Company.tab.pane',
   views:['Company.pane','Company.tree','Traceable.pane']
@@ -54,20 +52,18 @@ table 'Department-teams.table',
   actionMap:'masterDetail'
 
 action('addFromList',
-  parent:'lovOkFrontAction') {
-  next(parent:'addAnyToMasterFrontAction')
-}
+  parent:'lovOkFrontAction') { next(parent:'addAnyToMasterFrontAction') }
 
 listView('Team-teamMembers.list', preferredWidth:300, preferredHeight:300) {
   actionMap {
     actionList('EDIT'){
       action(parent:'lovAction',
-        custom:[
-          autoquery:false,
-          entityDescriptor_ref:'Employee',
-          initializationMapping:['company':'company'],
-          okAction_ref:'addFromList'
-        ]
+      custom:[
+      autoquery:false,
+      entityDescriptor_ref:'Employee',
+      initializationMapping:['company':'company'],
+      okAction_ref:'addFromList'
+      ]
       )
       action(ref:'removeAnyCollectionFromMasterFrontAction')
     }
@@ -77,40 +73,40 @@ listView('Team-teamMembers.list', preferredWidth:300, preferredHeight:300) {
 split_vertical('Departments.and.teams.view',
   cascadingModels:true,
   top:'Company-departments.table') {
-  bottom {
-    border (
+    bottom {
+      border (
       center:'Department-teams.table',
       east:'Team-teamMembers.list',
       cascadingModels:true
-    )
+      )
+    }
   }
-}
 
-split_vertical 'Company.organization.view',
+border 'Company.organization.view',
   model:'Company',
-  top:'Company.tab.pane',
-  bottom:'Departments.and.teams.view'
+  north:'Company.tab.pane',
+  center:'Departments.and.teams.view'
 
 image 'Employee-photo.pane',
   parent:'decoratedView',
   actionMap:'binaryPropertyActionMap'
 
-  
+
 form 'Employee.component.pane',
   columnCount:3
-  
+
 border('Employee.border.pane',
   center:'Employee.component.pane') {
-  north {
-    border {
-      east {
-        image model:'Employee-genderImageUrl',
+    north {
+      border {
+        east {
+          image model:'Employee-genderImageUrl',
           scrollable:false
+        }
       }
     }
   }
-}
-    
+
 split_horizontal 'Employee.pane',
   left:'Employee.border.pane',
   right:'Employee-photo.pane'
@@ -132,22 +128,22 @@ propertyView 'Event-text.pane',
 bean('Company.report',
   parent:'abstractReportDescriptor',
   custom:[
-    reportDesignUrl:'classpath:org/jspresso/hrsample/report/Company.jasper'
+  reportDesignUrl:'classpath:org/jspresso/hrsample/report/Company.jasper'
   ]) {
-  list('propertyDescriptors') {
-    bean(class:'org.jspresso.framework.model.descriptor.basic.BasicStringPropertyDescriptor',
+    list('propertyDescriptors') {
+      bean(class:'org.jspresso.framework.model.descriptor.basic.BasicStringPropertyDescriptor',
       name:'title'
-    )             
+      )
+    }
   }
-}
 
 bean('Company.chart',
   class:'org.jspresso.hrsample.chart.CompanyChart',
   custom:[
-    url:'classpath:com/fusioncharts/FCF_Column3D.swf',
-    title:'company.chart'
+  url:'classpath:com/fusioncharts/FCF_Column3D.swf',
+  title:'company.chart'
   ]
-)
+  )
 
 actionMap('Company-module-am'){
   actionList('FILE'){
@@ -155,15 +151,15 @@ actionMap('Company-module-am'){
     action(ref:'front.module.reload')
     action(parent:'staticReportAction',
       custom:[
-        reportDescriptor_ref:'Company.report'
+      reportDescriptor_ref:'Company.report'
       ]
-    )
+      )
     action(parent:'chartAction',
       description:'company.chart',
       custom:[
-        chartDescriptor_ref:'Company.chart'
+      chartDescriptor_ref:'Company.chart'
       ]
-    )
+      )
   }
 }
 
@@ -175,17 +171,16 @@ form 'City.module.view',
 split_vertical('Employee.module.view',
   actionMap:'beanModuleActionMap',
   top:'Employee.pane') {
-  bottom {
-    split_horizontal(
+    bottom {
+      split_horizontal(
       left:'Employee-events.table',
       right:'Event-text.pane',
       cascadingModels:true)
+    }
   }
-}
 
-split_vertical 'Company.module.view',
+border 'Company.module.view',
   parent:'Company.organization.view',
   actionMap:'Company-module-am'
-  
+
 messageSource(basenames:'org.jspresso.hrsample.i18n.Messages')
-  
