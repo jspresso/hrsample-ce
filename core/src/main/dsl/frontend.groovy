43df490ad59541c373be6fdaa94b7634bbdbee67
Vincent Vandenschrick <vvandens@jspresso.org>
2010-06-14 19:28:26 +0000

    [maven-release-plugin]  copy for tag hrsample-3.5.0

--- core/src/main/dsl/frontend.groovy
+++ core/src/main/dsl/frontend.groovy
@@ -1,43 +1,44 @@
 external id:['abstractFrontController',
-             'remotePeerRegistry',
-             'guidGenerator',
-             'connectorFactory',
-             'modelConnectorFactory',
-             'mvcBinder',
-             'translationProvider',
-             'modelCascadingBinder',
-             'iconFactory',
-             'actionFactory'
-             ]
+  'remotePeerRegistry',
+  'guidGenerator',
+  /*'connectorFactory',
+ 'modelConnectorFactory',
+ 'mvcBinder',
+ 'translationProvider',
+ 'modelCascadingBinder',
+ 'iconFactory',
+ 'actionFactory',*/
+  'abstractViewFactory'
+  ]
 
 workspace('masterdata.workspace',
   icon:'masterdata-48x48.png') {
-  nodeModule('masterdata.geography.module',
+    nodeModule('masterdata.geography.module',
     icon:'geography-48x48.png') {
-    filterModule('masterdata.cities.module',
+      filterModule('masterdata.cities.module',
       component:'City',
       detailView:'City.module.view')
+    }
   }
-}
 
 workspace('employees.workspace',
   icon:'people-48x48.png',
   grantedRoles:['administrator','staff-manager']) {
-  filterModule('employees.module',
+    filterModule('employees.module',
     icon:'employees-48x48.png',
     component:'Employee',
     detailView:'Employee.module.view',
     pageSize:4)
-}
+  }
 
 workspace('organization.workspace',
   icon:'structure-48x48.png',
   grantedRoles:['administrator','organization-manager']) {
-  filterModule('companies.module',
+    filterModule('companies.module',
     icon:'company-48x48.png',
     component:'Company',
     detailView:'Company.module.view')
-}
+  }
 
 action 'startupHrsampleAction',
   class:'org.jspresso.framework.application.frontend.action.module.ModuleSelectionAction',
@@ -50,30 +51,19 @@ controller 'hrsample.name',
   startup:'startupHrsampleAction',
   workspaces:['organization.workspace','employees.workspace','masterdata.workspace']
 
-bean 'abstractViewFactory', class:'org.jspresso.framework.view.AbstractViewFactory',
+bean 'viewFactoryBase', parent:'abstractViewFactory',
   custom: [
-    connectorFactory_ref:'connectorFactory',
-    modelConnectorFactory_ref:'modelConnectorFactory',
-    mvcBinder_ref:'mvcBinder',
-    translationProvider_ref:'translationProvider',
-    modelCascadingBinder_ref:'modelCascadingBinder',
-    iconFactory_ref:'iconFactory',
-    actionFactory_ref:'actionFactory',
-    lovAction_ref:'lovAction',
-    openFileAsBinaryPropertyAction_ref:'openFileAsBinaryPropertyAction',
-    saveBinaryPropertyAsFileAction_ref:'saveBinaryPropertyAsFileAction',
-    resetPropertyAction_ref:'resetPropertyFrontAction',
-    binaryPropertyInfoAction_ref:'binaryPropertyInfoAction',
-    defaultActionMapRenderingOptions:'LABEL_ICON']
-              
+  defaultActionMapRenderingOptions:'LABEL_ICON'
+  ]
+
 spec('remote-recording') {
   bean('frontController',
-    class:'org.jspresso.framework.application.frontend.controller.remote.RecordingRemoteController',
-    parent:'abstractFrontController',
-    custom:[
-      remotePeerRegistry_ref:'remotePeerRegistry',
-      guidGenerator_ref:'guidGenerator',
-      commandsFileName:'/tmp/commands.xml'
-    ]
+  class:'org.jspresso.framework.application.frontend.controller.remote.RecordingRemoteController',
+  parent:'abstractFrontController',
+  custom:[
+  remotePeerRegistry_ref:'remotePeerRegistry',
+  guidGenerator_ref:'guidGenerator',
+  commandsFileName:'/tmp/commands.xml'
+  ]
   )
 }
