36b424725312609afdea64207d36786cd30bf4a9
Vincent Vandenschrick <vvandens@jspresso.org>
2012-01-26 11:18:28 +0000

    Qooxdoo startup simplification. Cleanup strategy changed on browser close/reload (session is now reused)

--- startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/startup/qooxdoo/Application.js
+++ startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/startup/qooxdoo/Application.js
@@ -100,19 +100,18 @@ qx.Class.define("org.jspresso.hrsample.startup.qooxdoo.Application",
       if (qx.core.Environment.get("qx.debug")) {
         remoteController = new qx.io.remote.Rpc(
             "http://localhost:8080/hrsample-webapp/.qxrpc",
-            "org.jspresso.hrsample.startup.qooxdoo.QooxdooApplicationStartup"
+            "org.jspresso.hrsample.startup.remote.RemoteApplicationStartup"
         );
         remoteController.setCrossDomain(true);
       } else {
         remoteController = new qx.io.remote.Rpc(
             qx.io.remote.Rpc.makeServerURL(),
-            "org.jspresso.hrsample.startup.qooxdoo.QooxdooApplicationStartup"
+            "org.jspresso.hrsample.startup.remote.RemoteApplicationStartup"
         );
       }
       remoteController.setTimeout(600000);
-      
-      var qxController = new org.jspresso.framework.application.frontend.controller.qx.DefaultQxController(this, remoteController, "en");
-      qxController.start();
+
+      this.startController(remoteController);
     }
   }
 });
