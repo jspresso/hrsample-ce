db567681edf26c17d8b10bdfaad5f476a2fdaa50
Vincent Vandenschrick <vvandens@jspresso.org>
2012-09-03 10:08:38 +0000

    ASSIGNED - # 746: Make sure cloneInUnitOfWork is able to retrieve pre-existing Hibernate entities http://mantis.jspresso.org/view.php?id=746

--- core/src/test/java/org/jspresso/hrsample/backend/JspressoUnitOfWorkTests.java
+++ core/src/test/java/org/jspresso/hrsample/backend/JspressoUnitOfWorkTests.java
@@ -19,8 +19,24 @@
 package org.jspresso.hrsample.backend;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hibernate.Hibernate;
+import org.jspresso.framework.application.backend.persistence.hibernate.HibernateBackendController;
+import org.jspresso.framework.application.backend.session.EMergeMode;
+import org.jspresso.framework.model.persistence.hibernate.criterion.EnhancedDetachedCriteria;
+import org.jspresso.hrsample.model.Company;
+import org.jspresso.hrsample.model.Department;
 import org.junit.Test;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 
 /**
  * Unit Of Work management integration tests.
@@ -31,12 +47,62 @@ import org.junit.Test;
 public class JspressoUnitOfWorkTests extends BackTestStartup {
 
   /**
-   * Test 1.
+   * Test retrieve UOW cloned instance from session. See bug 746.
    */
   @Test
-  public void test1() {
-    System.out.println(getBackendController());
-    assertEquals(1, 1);
+  public void testClonedInstanceFromHibernateSession() {
+    final HibernateBackendController hbc = (HibernateBackendController) getBackendController();
+
+    EnhancedDetachedCriteria departmentCriteria = EnhancedDetachedCriteria
+        .forClass(Department.class);
+    final String updatedName = "testUpdatedName";
+    final List<Department> departments = hbc.findByCriteria(departmentCriteria,
+        EMergeMode.MERGE_KEEP, Department.class);
+    for (Department d : departments) {
+      d.setName(updatedName);
+    }
+
+    EnhancedDetachedCriteria companyCriteria = EnhancedDetachedCriteria
+        .forClass(Company.class);
+    final Company company = hbc.findFirstByCriteria(companyCriteria,
+        EMergeMode.MERGE_KEEP, Company.class);
+
+    hbc.getTransactionTemplate().execute(
+        new TransactionCallbackWithoutResult() {
+
+          @Override
+          protected void doInTransactionWithoutResult(
+              @SuppressWarnings("unused") TransactionStatus status) {
+            // Clone the company
+            Company companyClone = hbc.cloneInUnitOfWork(company);
+            // Check that the departments property is not initialized
+            assertFalse(Hibernate.isInitialized(companyClone
+                .straightGetProperty(Company.DEPARTMENTS)));
+            // Retrieve the company departments (load them in the Hibernate
+            // session)
+            Set<Department> hibernateSessionDepartments = companyClone
+                .getDepartments();
+
+            Map<Serializable, Department> sessionDepartmentsById = new HashMap<Serializable, Department>();
+            for (Department d : hibernateSessionDepartments) {
+              sessionDepartmentsById.put(d.getId(), d);
+            }
+
+            // Now clone each of the Jspresso session department and verify that
+            // there is no reference duplication, ie.e we are able to retrieve
+            // the Hibernate session instances.
+            List<Department> departmentClones = hbc
+                .cloneInUnitOfWork(departments);
+            for (Department d : departmentClones) {
+              Department sessionDepartment = sessionDepartmentsById.get(d
+                  .getId());
+              if (sessionDepartment != null) {
+                assertSame(sessionDepartment, d);
+                assertEquals(updatedName, sessionDepartment.getName());
+              }
+            }
+          }
+        });
   }
 
   /**
