e8004d1bf12be30c43ef0a06903dc8e2d1b8ea3a
Vincent Vandenschrick <vvandens@jspresso.org>
2012-10-30 09:01:02 +0000

    ASSIGNED - # 821: Find a way to have service delegates implement the service interface http://mantis.jspresso.org/view.php?id=821

--- core/src/main/java/org/jspresso/hrsample/model/service/EmployeeServiceDelegate.java
+++ core/src/main/java/org/jspresso/hrsample/model/service/EmployeeServiceDelegate.java
@@ -5,6 +5,7 @@ package org.jspresso.hrsample.model.service;
 
 import java.util.Date;
 
+import org.jspresso.framework.model.component.service.AbstractComponentServiceDelegate;
 import org.jspresso.framework.model.component.service.IComponentService;
 import org.jspresso.framework.util.i18n.ITranslationProvider;
 import org.jspresso.hrsample.model.Employee;
@@ -15,24 +16,24 @@ import org.jspresso.hrsample.model.Employee;
  * @version $LastChangedRevision$
  * @author Vincent Vandenschrick
  */
-public class EmployeeServiceDelegate implements IComponentService {
+public class EmployeeServiceDelegate extends
+    AbstractComponentServiceDelegate<Employee> implements EmployeeService, IComponentService {
 
   private ITranslationProvider translationProvider;
 
   /**
    * Computes the employee age.
    * 
-   * @param employee
-   *          the employee this service execution has been triggered on.
    * @param birthDate
    *          a birth date (might be different than the actual employee birth
    *          date).
    * @return the age computed from the birth date passed as parameter.
    */
-  public Integer computeAge(Employee employee, Date birthDate) {
+  @Override
+  public Integer computeAge(Date birthDate) {
     if (birthDate != null) {
-      return Integer.valueOf(
-          (int) ((new Date().getTime() - birthDate.getTime()) / (1000L * 60 * 60 * 24 * 365)));
+      return Integer
+          .valueOf((int) ((new Date().getTime() - birthDate.getTime()) / (1000L * 60 * 60 * 24 * 365)));
     }
     return null;
   }
@@ -40,11 +41,10 @@ public class EmployeeServiceDelegate implements IComponentService {
   /**
    * Gets the translationProvider.
    * 
-   * @param employee
-   *          the employee this service execution has been triggered on.
    * @return the translationProvider.
    */
-  public ITranslationProvider getTranslationProvider(Employee employee) {
+  @Override
+  public ITranslationProvider getTranslationProvider() {
     return translationProvider;
   }
 
