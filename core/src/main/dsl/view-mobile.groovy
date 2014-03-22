7549d741c8dde2efeb9f5b80d55c0dc7546e9fa4
Vincent Vandenschrick <vvandens@jspresso.org>
2014-03-22 07:32:19 +0000

    1138: Introduce mobile deployment channels for tablet / smartphone usage

--- core/src/main/dsl/view-mobile.groovy
+++ core/src/main/dsl/view-mobile.groovy
@@ -7,5 +7,54 @@ mobileListView('Employee.table.view')
 mobileCompositePage('Company.module.view') {
   sections {
     mobileForm()
+    mobileNavPage {
+      selection {
+        mobileListView(model:'Company-departments', actionMap:'masterActionMap')
+      }
+      nextPage {
+        mobileCompositePage(actionMap:'detailActionMap') {
+          sections {
+            mobileForm()
+            mobileNavPage {
+              selection {
+                mobileListView(model:'Department-teams', actionMap:'masterActionMap')
+              }
+              nextPage {
+                mobileCompositePage(actionMap:'detailActionMap') {
+                  sections {
+                    mobileForm()
+                    mobileNavPage {
+                      selection {
+                        mobileListView(model: 'Team-teamMembers', actionMap: 'masterActionMap')
+                      }
+                      nextPage {
+                        mobileCompositePage {
+                          sections {
+                            mobileForm()
+                          }
+                        }
+                      }
+
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    mobileNavPage {
+      selection {
+        mobileListView(model: 'Company-employees', actionMap: 'masterActionMap')
+      }
+      nextPage {
+        mobileCompositePage(actionMap: 'detailActionMap') {
+          sections {
+            mobileForm()
+          }
+        }
+      }
+    }
   }
 }
