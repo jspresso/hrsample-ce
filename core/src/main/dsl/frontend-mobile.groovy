455d87f90e1a25ff9e3cd05f5ed11984d42ac14d
Vincent Vandenschrick <vvandens@jspresso.org>
2014-03-27 08:55:14 +0000

    1138: Introduce mobile deployment channels for tablet / smartphone usage

--- core/src/main/dsl/frontend-mobile.groovy
+++ core/src/main/dsl/frontend-mobile.groovy
@@ -1,1 +1,54 @@
+workspace('masterdata.workspace',
+    icon:'masterdata-48x48.png') {
+  nodeModule('masterdata.geography.module',
+      icon:'geography-48x48.png') {
+    filterModule('masterdata.cities.module',
+        component:'City',
+        startup:'filterModuleStartup')
+  }
+}
+
+workspace('employees.workspace',
+    icon:'people-48x48.png',
+    grantedRoles:[
+        'administrator',
+        'staff-manager'
+    ]) {
+  filterModule('employees.module',
+      icon:'employees-48x48.png',
+      component:'Employee',
+      startup:'filterModuleStartup',
+      pageSize:4)
+}
+
+workspace('organization.workspace',
+    icon:'structure-48x48.png',
+    grantedRoles:[
+        'administrator',
+        'organization-manager'
+    ]) {
+  filterModule('companies.module',
+      icon:'company-48x48.png',
+      component:'Company',
+      detailView:'Company.module.view',
+      startup:'filterModuleStartup')
+}
+
+workspace('departments.workspace',
+    icon:'department-48x48.png') {
+  filterModule('departments.module',
+      icon:'department-48x48.png',
+      component:'Department')
+}
+
+controller 'hrsample.name',
+    icon:'people-48x48.png',
+    context:'hrsample',
+    language:'en',
+    startup:'startupHrsampleAction',
+    workspaces:[
+        'organization.workspace',
+        'employees.workspace',
+        'masterdata.workspace'
+    ]
 
--- core/src/main/dsl/view-mobile.groovy
+++ core/src/main/dsl/view-mobile.groovy
@@ -1,9 +1,3 @@
-mobileForm('City.module.view')
-
-mobileForm('Employee.module.view')
-
-mobileListView('Employee.table.view')
-
 mobileCompositePage('Company.module.view') {
   sections {
     mobileForm()
