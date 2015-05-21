be2de20bd2c65e42196f4b6e7184a6718d4ab2d0
Vincent Vandenschrick <vvandens@gmail.com>
2015-05-21 11:02:11 +0200

    hrsample theme simplification

--- startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/startup/qooxdoo/theme/Appearance.js
+++ startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/startup/qooxdoo/theme/Appearance.js
@@ -11,8 +11,28 @@
 qx.Theme.define("org.jspresso.hrsample.startup.qooxdoo.theme.Appearance",
 {
   extend : org.jspresso.framework.theme.Appearance,
-  
+
   appearances :
   {
+    "workspace-tree-folder/icon": {
+      base: true,
+      style: function (states) {
+        return {
+          height: 0,
+          width:0,
+          margin: [30, 0, 0, 20]
+        };
+      }
+    },
+
+    "accordion-section/bar": {
+      base: true,
+      style: function (states) {
+        return {
+          show: "label"
+        };
+      }
+    },
+
   }
 });
