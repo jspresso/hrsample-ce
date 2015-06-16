dee22747f6e4d1e6a00d05d9d113c48516296973
Vincent Vandenschrick <vvandens@gmail.com>
2015-06-16 12:01:40 +0200

    Migrated to qooxdoo 5.0

--- startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/startup/qooxdoo/MobileApplication.js
+++ startup/qooxdoo/client/src/main/js/org/jspresso/hrsample/startup/qooxdoo/MobileApplication.js
@@ -40,7 +40,7 @@ qx.Class.define("org.jspresso.hrsample.startup.qooxdoo.MobileApplication",
             "http://localhost:8080/hrsample-webapp/.qxrpc",
             "org.jspresso.hrsample.startup.remote.RemoteMobileApplicationStartup"
         );
-        //remoteController.setCrossDomain(true);
+        remoteController.setCrossDomain(true);
       } else {
         remoteController = new qx.io.remote.Rpc(
             qx.io.remote.Rpc.makeServerURL(),
