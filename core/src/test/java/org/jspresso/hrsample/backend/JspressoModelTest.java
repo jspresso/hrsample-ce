f29aa8dab313b65fa52bb37006faeb1cbda8d404
Vincent Vandenschrick <vvandens@gmail.com>
2015-10-02 14:31:32 +0200

    Fixed test

--- core/src/test/java/org/jspresso/hrsample/backend/JspressoModelTest.java
+++ core/src/test/java/org/jspresso/hrsample/backend/JspressoModelTest.java
@@ -706,7 +706,8 @@ public class JspressoModelTest extends BackTestStartup {
         Company company = hbc.getEntityFactory().createEntityInstance(Company.class);
         company.setName("testComp");
         hbc.registerForUpdate(company);
-        assertNull("Create timestamp assigned too early", company.getCreateTimestamp());
+        // Old behaviour restored
+        //assertNull("Create timestamp assigned too early", company.getCreateTimestamp());
         company.setName("testCompMod");
         return company.getId();
       }
