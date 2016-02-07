8ffc53e4ed7cff0c3fe03449ae502c6790bccba7
Maxime HAMM <maxime.hamm@laposte.net>
2016-02-07 22:35:22 +0100

    adding one more filter criteria for Employee entity

--- core/src/main/dsl/model.groovy
+++ core/src/main/dsl/model.groovy
@@ -69,7 +69,8 @@ Entity('Employee',
                 'firstName',
                 'gender',
                 'birthDate',
-                'company']) {
+                'company', 
+                'managedOu.manager']) {
   string_32 'firstName', mandatory: true, processors: 'FirstNameProcessor', translatable: true
   string_10 'ssn', regex: "[\\d]{10}", regexSample: '0123456789', unicityScope: 'empSsn'
   date 'birthDate', processors: 'BirthDateProcessor'
